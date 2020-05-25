f63395c839bf86d2fbb7220cc5e416fd774f5fe6
FernandoBatoni <61693494+FernandoBatoni@users.noreply.github.com>
2020-05-24 22:59:26 -0300

    Add files via upload

---
+++ Atividade 7/Atividade7.1.js
@@ -0,0 +1,8 @@
+var Retangulo = new Object();
+            Retangulo.X;
+            Retangulo.Y;
+    
+        Retangulo.X = prompt("Digite o nome a base:")
+        Retangulo.Y = prompt("Digite o nome a altura:")
+
+        alert("A área do retângulo é: " +  Retangulo.X *  Retangulo.Y)
