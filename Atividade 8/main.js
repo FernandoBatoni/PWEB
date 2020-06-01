7009dd331d464bb61e62882f25dd4c04ab11be14
FernandoBatoni <61693494+FernandoBatoni@users.noreply.github.com>
2020-06-01 12:02:09 -0300

    Add files via upload

---
+++ Atividade 8/main.js
@@ -0,0 +1,11 @@
+function mOver(obj) {
+    obj.src = "imagens/janelaaberta.png";
+}
+
+function mOut(obj) {
+    obj.src = "imagens/janelafechada.png";
+}
+
+function mClick(obj) {
+    obj.src = "imagens/janelaquebrada.png";
+}
